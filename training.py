ad098db2f755905fb90fb99543c7b076b9376149
markopisacic <m.pisacic5@gmail.com>
2020-05-25 20:35:51 +0200

    Modified training.py so that training doesn't fail so often.

--- training.py
+++ training.py
@@ -4,65 +4,70 @@ from flair.data import Corpus
 from flair.datasets import ColumnCorpus
 
 # define columns
-from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings, FlairEmbeddings
+from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings, FlairEmbeddings, TransformerWordEmbeddings
 
-columns = {0: 'text', 1: 'propaganda'}
+if __name__ == '__main__':
+    columns = {0: 'text', 1: 'propaganda'}
 
-# this is the folder in which train, test and dev files reside
-data_folder = './data'
+    # this is the folder in which train, test and dev files reside
+    data_folder = './data'
 
-# init a corpus using column format, data folder and the names of the train, dev and test files
-corpus: Corpus = ColumnCorpus(data_folder, columns,
-                              train_file='train.txt',
-                              test_file='test.txt',
-                              dev_file='validate.txt').downsample(0.4)
-print(corpus)
+    # init a corpus using column format, data folder and the names of the train, dev and test files
+    corpus: Corpus = ColumnCorpus(data_folder, columns, in_memory=False,
+                                  train_file='train.txt',
+                                  test_file='test.txt',
+                                  dev_file='validate.txt')
+    corpus.filter_empty_sentences()
+    print(corpus)
 
-tag_type = 'propaganda'
-tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
-print(tag_dictionary)
+    tag_type = 'propaganda'
+    tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
+    print(tag_dictionary)
 
-embedding_types: List[TokenEmbeddings] = [
+    embedding_types: List[TokenEmbeddings] = [
 
-    WordEmbeddings('glove'),
+        WordEmbeddings('glove'),
 
-    # CharacterEmbeddings(),
+        # other embeddings
 
-    # FlairEmbeddings('news-forward'),
-    # FlairEmbeddings('news-backward'),
-]
-embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
+        # CharacterEmbeddings(),
+        # TransformerWordEmbeddings('bert-base-cased'),
+        # FlairEmbeddings('news-forward'),
+        # FlairEmbeddings('news-backward'),
+    ]
+    embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
 
-# 5. initialize sequence tagger
-from flair.models import SequenceTagger
+    # 5. initialize sequence tagger
+    from flair.models import SequenceTagger
 
-tagger: SequenceTagger = SequenceTagger(hidden_size=256,
-                                        embeddings=embeddings,
-                                        tag_dictionary=tag_dictionary,
-                                        tag_type=tag_type,
-                                        use_crf=True)
+    tagger: SequenceTagger = SequenceTagger(hidden_size=256,
+                                            embeddings=embeddings,
+                                            tag_dictionary=tag_dictionary,
+                                            tag_type=tag_type,
+                                            use_crf=True)
 
-# 6. initialize trainer
-from flair.trainers import ModelTrainer
+    # 6. initialize trainer
+    from flair.trainers import ModelTrainer
 
-trainer: ModelTrainer = ModelTrainer(tagger, corpus)
+    trainer: ModelTrainer = ModelTrainer(tagger, corpus)
 
-# 7. start training
-trainer.train('resources/taggers/propaganda',
-              learning_rate=0.1,
-              mini_batch_size=20,
-              max_epochs=2,
-              checkpoint=True)
+    # 7. start training
+    trainer.train('resources/taggers/propaganda',
+                  learning_rate=0.1,
+                  mini_batch_size=20,
+                  max_epochs=20,
+                  checkpoint=True)
 
-# 8. plot weight traces (optional)
-from flair.visual.training_curves import Plotter
-plotter = Plotter()
-plotter.plot_weights('resources/taggers/propaganda/weights.txt')
+    # 8. plot weight traces (optional)
+    from flair.visual.training_curves import Plotter
 
+    plotter = Plotter()
+    plotter.plot_weights('resources/taggers/propaganda/weights.txt')
+    plotter.plot_training_curves('resources/taggers/propaganda/loss.tsv')
 
-# to be plotted after training
+    # to be plotted after training
 
-# from flair.visual.training_curves import Plotter
-# plotter = Plotter()
-# plotter.plot_training_curves('loss.tsv')
-# plotter.plot_weights('weights.txt')
+    # from flair.visual.training_curves import Plotter
+    # plotter = Plotter()
+    # plotter.plot_training_curves('loss.tsv')
+    # plotter.plot_weights('weights.txt')
