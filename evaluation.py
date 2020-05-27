1cb2cf4f19e833582d2b9624bc8909a4395a6ceb
pisacicmarko <marko.pisacic@fer.hr>
2020-05-27 17:28:31 +0200

    Adjusted evaluation script

--- evaluation.py
+++ evaluation.py
@@ -1,24 +1,23 @@
+from pathlib import Path
 from flair.data import  Corpus
-from flair.datasets import ColumnCorpus, DataLoader
+from flair.datasets import ColumnCorpus, DataLoader, ColumnDataset
 from flair.models import SequenceTagger
 import torch
 
 torch.cuda.empty_cache()
 
 MODEL_PATH = './resources/taggers/propaganda/best-model.pt'
+TEST_PATH = './data/test.txt'
 OUT_PATH = './resources/taggers/propaganda/eval.out'    # uncomment to save predictions for each sample to file
+
+columns = {0: 'text', 1: 'propaganda', 2: 'doc_id', 3: 'sentence_id'}
 
 model : SequenceTagger = SequenceTagger.load(MODEL_PATH)
 
-columns = {0: 'text', 1: 'propaganda'}
-data_folder = './data'
+test = ColumnDataset(Path(TEST_PATH), columns)
+test = Corpus._filter_empty_sentences(test)
 
-corpus : Corpus = ColumnCorpus(data_folder, columns, in_memory=False,
-                               train_file='train.txt',
-                               test_file='test.txt',
-                               dev_file='validate.txt').downsample(0.2)
-
-res, e = model.evaluate(DataLoader(corpus.test), OUT_PATH)
+res, e = model.evaluate(DataLoader(test), OUT_PATH)
 
 print("Main score: " + str(res.main_score) + ", loss: " + str(e))
 print(res.log_header)
