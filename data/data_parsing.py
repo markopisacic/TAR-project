59027690ea6cc57cbc7aa3ce4b3aadcb8ea09e11
pisacicmarko <marko.pisacic@fer.hr>
2020-05-28 23:51:01 +0200

    Corrected data_parsing script (last one-word lines + wrong offsets)

--- data/data_parsing.py
+++ data/data_parsing.py
@@ -25,28 +25,41 @@ def is_in_span(spans, pos):
     return 0
 
 
+def filter_doubles(l):
+    n = []
+    for i in range(len(l) - 1):
+        if l[i] != l[i + 1] - 1:
+            n.append(l[i])
+    n.append(l[-1])
+    return n
+
+
 for doc_idx, filename in enumerate(os.listdir(LABELS_DIR)):
     spans = []
     with open(LABELS_DIR + '/' + filename) as file:
         for line in file:
             spans.append((int(line.split()[1]), int(line.split()[2])))
-    spans = sorted(spans, key = lambda x: x[0])
+    spans = sorted(spans, key=lambda x: x[0])
 
-    with open(ARTICLES_DIR + '/' + filename) as file: text = file.read()
+    with open(ARTICLES_DIR + '/' + filename) as file:
+        text = file.read()
     labels = []
     for w, o1, o2 in split_with_offsets(text):
         labels.append((w, o1, is_in_span(spans, (o1 + o2) / 2)))
 
-    text = os.linesep.join([s for s in text.splitlines() if s])
     newlines = [m.start() for m in re.finditer('\n', text)]
+    newlines = filter_doubles(newlines)
+    if newlines[-1] != len(text):
+        newlines.append(len(text))
+
     out_file = open(OUT_DIR + '/' + filename, "w")
     current_newline_idx = 0
-    for w,o,l in labels:
+    for w, o, l in labels:
         if o > newlines[current_newline_idx]:
-            current_newline_idx = current_newline_idx+1 if current_newline_idx < len(newlines)-1 else len(newlines)-1
+            current_newline_idx = current_newline_idx + 1 if current_newline_idx < len(newlines) - 1 else len(
+                newlines) - 1
             out_file.write('\n')
         out_file.write(w + " " + str(l) + " " + str(doc_idx) + " " + str(current_newline_idx) + "\n")
     out_file.close()
 
-
-
+print('Done.')
