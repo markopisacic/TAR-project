4fe4a3caf17f388438a2014a59d162b4956dac8b
Luka Barišić <luka.barisic97@gmail.com>
2020-05-30 19:25:29 +0200

    Add LIWC article data and LIWC custom embedders

--- custom_embeddings.py
+++ custom_embeddings.py
@@ -37,3 +37,60 @@ class NoContextBertEmbeddings(TokenEmbeddings):
                     token.set_embedding(self.name, torch.zeros(self.embedding_length, dtype=torch.float32))
 
         return sentences
+
+class LIWC2007Embeddings(TokenEmbeddings):
+    def __init__(self):
+        self.name = 'LIWC2007Embeddings'
+        self.static_embeddings = True
+
+        # TODO load with pickle
+        self.embeddings = pickle.load(open('liwc2007_embeddings.pickle', 'rb'))
+        self.tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+        self.__embedding_length: int = 64
+        super().__init__()
+
+    @property
+    def embedding_length(self) -> int:
+        return self.__embedding_length
+
+    def _add_embeddings_internal(self, sentences: List[Sentence]) -> List[Sentence]:
+
+        for i, sentence in enumerate(sentences):
+
+            for token in sentence:
+                doc_id = token.get_tag('doc_id').value
+                if doc_id in self.embeddings:
+                    token.set_embedding(self.name, self.embeddings[doc_id])
+                else:
+                    token.set_embedding(torch.zeros(self.embedding_length, dtype = torch.float32))
+
+        return sentences
+
+class LIWC2015Embeddings(TokenEmbeddings):
+    def __init__(self):
+        self.name = 'LIWC2015Embeddings'
+        self.static_embeddings = True
+
+        # TODO load with pickle
+        self.embeddings = pickle.load(open('liwc2015_embeddings.pickle', 'rb'))
+        self.tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+        self.__embedding_length: int = 73
+        super().__init__()
+
+    @property
+    def embedding_length(self) -> int:
+        return self.__embedding_length
+
+    def _add_embeddings_internal(self, sentences: List[Sentence]) -> List[Sentence]:
+
+        for i, sentence in enumerate(sentences):
+
+            for token in sentence:
+                doc_id = token.get_tag('doc_id').value
+                if doc_id in self.embeddings:
+                    token.set_embedding(self.name, self.embeddings[doc_id])
+                else:
+                    token.set_embedding(torch.zeros(self.embedding_length, dtype = torch.float32))
+
+        return sentences
+
